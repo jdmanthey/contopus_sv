8786fa75f9e68b37db9fffab4a37d573b141ef63
Joe Manthey <jdmanthey@gmail.com>
2024-08-06 17:10:22 -0500

    Update 01_convert_bam_to_fastq.sh

--- 01_convert_bam_to_fastq.sh
+++ 01_convert_bam_to_fastq.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=convert
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+
 source activate pacbio
 
-bam2fasta -o C_sordidulus__KU39667 m84046_240803_051617_s4.hifi_reads.bc2039.bam
+bam2fasta -o C_sordidulus__KU39667 m84046_240803_051617_s4.hifi_reads.bc2039.bam 
+
+
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=convert
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+
+source activate pacbio
 
 bam2fasta -o C_virens__KU39615 m84046_240803_051617_s4.hifi_reads.bc2040.bam
