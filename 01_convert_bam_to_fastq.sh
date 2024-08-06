4d88578822ca6adba4ea5ba43ee552d30f50a3a4
Joe Manthey <jdmanthey@gmail.com>
2024-08-06 16:41:25 -0500

    Update 01_convert_bam_to_fastq.sh

--- 01_convert_bam_to_fastq.sh
+++ 01_convert_bam_to_fastq.sh
@@ -1,8 +1,5 @@
+source activate pacbio
 
-bedtools bamtofastq -i m84046_240803_051617_s4.hifi_reads.bc2039.bam -fq C_sordidulus__KU39667.fastq
+bam2fasta -o C_sordidulus__KU39667 m84046_240803_051617_s4.hifi_reads.bc2039.bam
 
-bedtools bamtofastq -i m84046_240803_051617_s4.hifi_reads.bc2040.bam -fq C_virens__KU39615.fastq
-
-gzip C_sordidulus__KU39667.fastq
-
-gzip C_virens__KU39615.fastq
+bam2fasta -o C_virens__KU39615 m84046_240803_051617_s4.hifi_reads.bc2040.bam
