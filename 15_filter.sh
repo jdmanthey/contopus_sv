4b19040ac3c6579cfa0ed3d36ece2cbe64238023
Joe Manthey <jdmanthey@gmail.com>
2024-09-25 11:27:38 -0500

    Update 15_filter.sh

--- 15_filter.sh
+++ 15_filter.sh
@@ -12,17 +12,35 @@ source activate bcftools
 # define main working directory
 workdir=/lustre/scratch/jmanthey/18_contopus_sv/07_merged_vcf
 
+## indels
 # filter based on missing data (max missing = 3/35) and MAC > 0 
 vcftools --vcf ${workdir}/all_sequence_variants.vcf --max-missing 0.9 \
---max-maf 0.49 --mac 1 --recode --recode-INFO-all \
---out ${workdir}/all_sequence_variants_filtered
+--max-maf 0.49 --mac 1 --max-alleles 2 --keep-only-indels --recode --recode-INFO-all \
+--out ${workdir}/indels_filtered
 
 # filter based on missing data (max missing = 3/35) and MAC > 9 
 vcftools --vcf ${workdir}/all_sequence_variants.vcf --max-missing 0.9 \
---max-maf 0.49 --mac 9 --recode --recode-INFO-all \
---out ${workdir}/all_sequence_variants_filtered_mac10
+--max-maf 0.49 --mac 9 --max-alleles 2 --keep-only-indels --recode --recode-INFO-all \
+--out ${workdir}/indels_filtered_mac10
 
 # filter based on missing data (no missing) and MAC > 9 
 vcftools --vcf ${workdir}/all_sequence_variants.vcf --max-missing 1.0 \
---max-maf 0.49 --mac 9 --recode --recode-INFO-all \
---out ${workdir}/all_sequence_variants_filtered_mac10_complete
+--max-maf 0.49 --mac 9 --max-alleles 2 --keep-only-indels --recode --recode-INFO-all \
+--out ${workdir}/indels_filtered_mac10_complete
+
+
+## snps
+# filter based on missing data (max missing = 3/35) and MAC > 0 
+vcftools --vcf ${workdir}/all_sequence_variants.vcf --max-missing 0.9 \
+--max-maf 0.49 --mac 1 --max-alleles 2 --remove-indels --recode --recode-INFO-all \
+--out ${workdir}/snps_filtered
+
+# filter based on missing data (max missing = 3/35) and MAC > 9 
+vcftools --vcf ${workdir}/all_sequence_variants.vcf --max-missing 0.9 \
+--max-maf 0.49 --mac 9 --max-alleles 2 --remove-indels --recode --recode-INFO-all \
+--out ${workdir}/snps_filtered_mac10
+
+# filter based on missing data (no missing) and MAC > 9 
+vcftools --vcf ${workdir}/all_sequence_variants.vcf --max-missing 1.0 \
+--max-maf 0.49 --mac 9 --max-alleles 2 --remove-indels --recode --recode-INFO-all \
+--out ${workdir}/snps_filtered_mac10_complete
